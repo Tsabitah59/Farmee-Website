3c6d2f63e74c6ca34d27cf27022c926e8af97693
Tsabitah59 <tsabitahsafa06@gmail.com>
2024-09-24 13:08:53 +0700

    adding several emty code again and more js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-let hero_title = document.getElementById('title-hero');
+let hero_right = document.getElementById('hero-right');  
+let hero_left = document.getElementById('hero-left-title');
 
-window.addEventListener('scroll', function(){
-    let value = window.scrollY;
-    hero_title.style.transform = 'translateX(' + value * -0.5 + 'px)';
+window.addEventListener('scroll', function(){  
+    let value = window.scrollY;  
+    hero_left.style.transform = 'translateX(' + value * -0.5 + 'px)'; 
+    hero_right.style.transform = 'translateX(' + value * -0.5 + 'px)';  
+})
+
+let valueDisplays = document.querySelectorAll('.num');
+let interval = 400;
+
+valueDisplays.forEach((valueDisplays) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute('data-val'));
+    let duration = Math.floor(interval/  endValue);
+    let counter = setInterval(function() {
+        startValue += 1;
+        valueDisplays.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration)
 })
