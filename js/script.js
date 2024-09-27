9b28eba313c949927cc97220b129bc71aec30397
Tsabitah59 <tsabitahsafa06@gmail.com>
2024-09-27 16:31:48 +0700

    Adding a lot of page, but THERE ARE ERRORS PLEASE

--- js/script.js
+++ js/script.js
@@ -1,24 +1,48 @@
-let hero_right = document.getElementById('hero-right');  
-let hero_left = document.getElementById('hero-left-title');
+// Parallax
+let hero_right = document.getElementById("hero-right");
+let hero_left = document.getElementById("hero-left-title");
 
-window.addEventListener('scroll', function(){  
-    let value = window.scrollY;  
-    hero_left.style.transform = 'translateX(' + value * -0.5 + 'px)'; 
-    hero_right.style.transform = 'translateX(' + value * -0.5 + 'px)';  
-})
+window.addEventListener("scroll", function () {
+  let value = window.scrollY;
+  hero_left.style.transform = "translateX(" + value * -0.5 + "px)";
+  hero_right.style.transform = "translateX(" + value * -0.5 + "px)";
+});
 
-let valueDisplays = document.querySelectorAll('.num');
+// Counter
+let valueDisplays = document.querySelectorAll(".num");
 let interval = 400;
 
-valueDisplays.forEach((valueDisplays) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplays.getAttribute('data-val'));
-    let duration = Math.floor(interval/  endValue);
-    let counter = setInterval(function() {
-        startValue += 1;
-        valueDisplays.textContent = startValue;
-        if (startValue == endValue) {
-            clearInterval(counter);
-        }
-    }, duration)
-})
+const startCounting = (valueDisplays) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue)
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplays.textContent = startValue
+    if (startValue === endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+};
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      valueDisplays.forEach(startCounting)
+      observer.unobserve(entry.target)
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, options);
+
+valueDisplays.forEach((valueDisplay) => {
+  observer.observe(valueDisplay)
+});
+
